1647c45103be10cdf4aa3d583fe02d21d1cd1b73
ayankhan <ayankhan8223@gmail.com>
2022-06-24 18:23:40 +0530

    finally all things done and responsive

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
+import "./App.css";
+import Sidebar from "./Components/Sidebar/Sidebar";
+import Content from "./Components/Content/Content";
+const { useState } = React;
+
+const App = () => {
+  const [showSidebar, onSetShowSidebar] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex">
+      <Sidebar
+        onSidebarHide={() => {
+          onSetShowSidebar(false);
+        }}
+        showSidebar={showSidebar}
+      />
+      <Content
+        onSidebarHide={() => {
+          onSetShowSidebar(true);
+        }}
+      />
     </div>
   );
-}
+};
 
 export default App;
